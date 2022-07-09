e4895aac015be537dae94011a5cf8a7c61eac8c7
paulozterra <66433825+paulozterra@users.noreply.github.com>
2022-07-09 16:31:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask
-from processingrtree import ind, data_vectors
+import csv
+
+from processingrtree import ind
 from queue import PriorityQueue
 from scipy.spatial import distance
+from flask import Flask
+
+
 
 app = Flask(__name__)
 
-
+data_vectors = []
+with open('data_vector.csv','r') as csv_file:
+    csv_reader = csv.reader(csv_file,delimiter=',')
+    for data in csv_reader:
+        data = [float(x) for x in data[1:]]
+        data_vectors.append(data)
 @app.route('/')
 def hello():
     return 'hola mongol'
 
+@app.route('/1')
 def KNNsearch(Query, k):
     result = []
     priority = PriorityQueue()
@@ -19,13 +29,14 @@ def KNNsearch(Query, k):
     for i in range(k):
         result.append(priority.get())
     return result
-
+@app.route('/2')
 def Rtree(Query, k):
     result = []
     for p in ind.nearest(Query, num_results=k):
         result.append(p)
     return result
 
+@app.route('/3')
 def Rangesearch(Query, radio):
     result = []
     for vector in data_vectors:
